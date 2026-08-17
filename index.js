3c57cd0458ccacedf69b525c8c82275c82e1c1f1
Soham kulkarni <sohamvkulkarni05@gmail.com>
2026-08-17 23:39:39 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+//First image
+let randomNum1 = Math.floor(Math.random()*6) + 1;
+let randomImage1 = "images/" + "dice"+ randomNum1 + ".png";
+
+let image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src",randomImage1);
+
+//second image
+
+let randomNum2 = Math.floor(Math.random()*6) + 1;
+let randomImage2 = "images/" + "dice" + randomNum2 + ".png";
+let image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src",randomImage2);
+
+if(randomNum1 > randomNum2)
+{
+    document.querySelector("h1").innerHTML="Player 1 wins!";
+}
+else if(randomNum2 > randomNum1)
+{
+    document.querySelector("h1").innerHTML="Player 2 wins!";
+}
+else
+{
+    document.querySelector("h1").innerHTML="Draw";
+}
